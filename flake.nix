f0cf61c111ea2176c8eb553d1a794d8c26547492
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-06-15 23:51:26 +0300

    build(nix): set build

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Python program to generate line artwork.";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "genLineArt";
+      shell = ./shell.nix;
+    };
+}
