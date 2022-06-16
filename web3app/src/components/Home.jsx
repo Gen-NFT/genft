69db2faca4d85422cdc71b688bbac786fdd8f822
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-06-16 17:22:45 +0300

    feat(webApp): add `web3App components` beta.

---
+++ web3app/src/components/Home.jsx
@@ -0,0 +1,93 @@
+import WalletBalance from './WalletBalance';
+import { useEffect, useState } from 'react';
+
+import { ethers } from 'ethers';
+import GeNFT from '../../artifacts/contracts/GeNFT.sol/GeNFT.json';
+
+const contractAddress = 'Some Address';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// get the end user
+const signer = provider.getSigner();
+
+// get the smart contract
+const contract = new ethers.Contract(contractAddress, GeNFT.abi, signer);
+
+function Home() {
+
+  const [totalMinted, setTotalMinted] = useState(0);
+  useEffect(() => {
+    getCount();
+  }, []);
+
+  const getCount = async () => {
+    const count = await contract.count();
+    console.log(parseInt(count));
+    setTotalMinted(parseInt(count));
+  };
+
+  return (
+    <div>
+        <WalletBalance />
+
+        {Array(totalMinted + 1)
+        .fill(0)
+        .map((_, i) => (
+            <div key={i}>
+            <NFTImage tokenId={i} getCount={getCount} />
+            </div>
+        ))}
+    </div>
+  );
+}
+
+function NFTImage({ tokenId, getCount }) {
+  const contentId = 'Some CID';
+  const metadataURI = `${contentId}/${tokenId}.json`;
+  const imageURI = `https://gateway.pinata.cloud/ipfs/${contentId}/${tokenId}.png`;
+  const [isMinted, setIsMinted] = useState(false);
+  useEffect(() => {
+    getMintedStatus();
+  }, [isMinted]);
+
+  const getMintedStatus = async () => {
+    const result = await contract.isContentOwned(metadataURI);
+    console.log(result)
+    setIsMinted(result);
+  };
+
+  const mintToken = async () => {
+    const connection = contract.connect(signer);
+    const addr = connection.address;
+    const result = await contract.payToMint(addr, metadataURI, {
+      value: ethers.utils.parseEther('120'),
+    });
+
+    await result.wait();
+    getMintedStatus();
+    getCount();
+  };
+
+  async function getURI() {
+    const uri = await contract.tokenURI(tokenId);
+    alert(uri);
+  }
+  return (
+    <div>
+      <img width="400"  src={isMinted ? imageURI : `https://gateway.pinata.cloud/ipfs/${contentId}/${tokenId}.png`}></img>
+        <h5>ID #{tokenId}</h5>
+        {!isMinted ? (
+          <button onClick={mintToken}>
+            Mint
+          </button>
+        ) : (
+          <button onClick={getURI}>
+            Taken! Show URI
+          </button>
+        )}
+    </div>
+  );
+}
+
+export default Home;
