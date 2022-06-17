c8805d28304560bcb545c45aff9ba0ad6e2cec7e
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-06-17 12:33:01 +0300

    Deploy to Ropsten

--- web3app/scripts/geNFT-deploy.js
+++ web3app/scripts/geNFT-deploy.js
@@ -1,15 +1,17 @@
 const hre = require("hardhat");
 
 async function main() {
-
+  console.log("Getting contract");
+  const signerAddress = (await hre.ethers.getSigner()).getAddress();
+  console.log({ signerAddress });
   const GenArtNFT = await hre.ethers.getContractFactory("GeNFT");
   const genArtNFT = await GenArtNFT.deploy();
+  console.log("Deploying contract");
 
   await genArtNFT.deployed();
 
   console.log("My NFT deployed to:", genArtNFT.address);
 }
-
 
 main()
   .then(() => process.exit(0))
